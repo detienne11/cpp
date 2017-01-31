f1eddfd03f35ef25a8330d342fb098b738825e2d
Neo <Neo@jupiter>
2017-01-31 21:58:30 +0100

    sample project

---
+++ sample/test.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : test2.cpp
+// Author      : David
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf("Hello World!!!");
+	return EXIT_SUCCESS;
+}
